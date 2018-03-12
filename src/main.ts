0b7977d6b59581c7ac56e4299003e8a1c2bc7967
Owen <owen@glosfer.com>
2018-03-12 15:42:42 +0900

    Poll chain length, wait 3 confirmations before processing

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,13 @@ let db = mysql.createConnection({
 });
 
 let txInsertCount = 0
-function insertTx(txid: string, address: string, amount: BigNumber | Number) {
-    let sql = "INSERT INTO `input_transactions` (`tx_hash`, `address`, `tx_amount`) VALUES (?,?,?);"
-    db.query(sql, [txid, address, amount], (error, results, fields) => {
+function insertTx(txid: string, address: string, amount: BigNumber | Number, time: Date) {
+    let sql = "INSERT INTO `input_transactions` (`tx_hash`, `address`, `tx_amount`, `flag`,`time`) VALUES (?,?,?,?,?);"
+    db.query(sql, [txid, address, amount.toString(), "eth", time], (error, results, fields) => {
         if (error) {
             console.log(`Failed to insert TXID ${txid}, ${address}, ${amount}: ${error}`)
         } else {
+            // console.log(`Insert TXID ${txid}: ${address}, ${amount}`)
             txInsertCount++
         }
     })
@@ -25,10 +26,10 @@ function insertTx(txid: string, address: string, amount: BigNumber | Number) {
 }
 
 
-let timerId: NodeJS.Timer;
-let address = "0x390dE26d772D2e2005C6d1d24afC902bae37a4bB"
-address = "0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"
+
+let addresses = ["0x390dE26d772D2e2005C6d1d24afC902bae37a4bB", "0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98"]
 async function main() {
+    addresses = addresses.map((str) => str.toLowerCase())
     await new Promise((resolve, reject) => {
         db.connect((err) => {
             if (err) {
@@ -48,53 +49,53 @@ async function main() {
 
 
     web3.eth.getSyncing(printSync)
-    let balance = web3.eth.getBalance(address);
-    console.log(`'${address}': ${web3.fromWei(balance, "ether")} Eth`)
 
-    let sub = web3.eth.filter("latest");
-    sub.watch((err, result) => {
-        if (err) {
-            console.log(err)
-        }
-        if (typeof result == 'string') {
-            let block = web3.eth.getBlock(result)
-            console.log(`Block #${block.number}: ${block.hash} ${block.transactions.length}`)
-            for (let i = 0; i < block.transactions.length; i++) {
-                let tx = web3.eth.getTransactionFromBlock(result, i)
-                if (true || tx.from == address || tx.to == address) {
-                    console.log(`TX# ${tx.from} -- ${web3.fromWei(tx.value, 'ether')} --> ${tx.to}`)
-                    insertTx(tx.hash, tx.from, web3.fromWei(tx.value, 'ether'))
-                }
-                break;
-            }
-        } else {
-            console.log(result)
-        }
-    })
+    setInterval(() => web3.eth.getBlockNumber(blockPoll), 1000)
 
-    // let sub2 = web3.eth.filter({ address: '0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98' });
-    // sub2.watch((err, result) => {
-    //     console.log(`${result.blockNumber}: ${result.transactionHash} ${result.topics.join(", ")}`)
-    // })
 }
 
+let lastBlockNumber: number;
+let confirmations = 3;
+function blockPoll(err: Error, blockNumber: number) {
+    if (err) {
+        console.log("Could not get blockHeight");
+        return
+    }
+    if (lastBlockNumber == undefined) {
+        lastBlockNumber = blockNumber - (confirmations + 1)
+    }
+    if (lastBlockNumber + confirmations < blockNumber) {
+        lastBlockNumber++
+        setImmediate(processBlock, lastBlockNumber)
+    }
+}
+
+function processBlock(number: number) {
+    let block = web3.eth.getBlock(number, true)
+    console.log(`Block #${block.number}: ${block.hash} ${block.transactions.length}`)
+    for (let tx of block.transactions) {
+        if (addresses.find((address) => tx.from == address || tx.to == address) !== undefined) {
+            console.log(`TX# ${tx.from} -- ${web3.fromWei(tx.value, 'ether')} --> ${tx.to}`)
+            setImmediate(insertTx, tx.hash, tx.from, web3.fromWei(tx.value, 'ether'), new Date(block.timestamp * 1000))
+
+        }
+    }
+}
+
+let printTimerId: NodeJS.Timer;
 function printSync(err: Error, res: Web3.SyncingResult) {
     if (err) {
         console.log(err)
-        clearInterval(timerId)
+        clearInterval(printTimerId)
     }
     if (res === false) {
         console.log(`Sync complete`)
-        clearInterval(timerId)
+        clearInterval(printTimerId)
     } else {
-        if (timerId === undefined) {
+        if (printTimerId === undefined) {
             setInterval(() => web3.eth.getSyncing(printSync), 1000)
         }
         console.log(`Sync: ${res.currentBlock} / ${res.highestBlock} (${100 * res.currentBlock / res.highestBlock}%)`)
-        console.log(`TXs inserted: ${txInsertCount}`)
-        let balance = web3.eth.getBalance(address);
-        console.log(`'${address}': ${web3.fromWei(balance, "ether")} Eth`)
-
     }
 }
 
