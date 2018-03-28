dd9e0df36d24c1622e193f125c5d4e19da4456ce
Owen <owen@glosfer.com>
2018-03-28 19:41:47 +0900

    Insert new addresses into

--- src/generateAddresses.ts
+++ src/generateAddresses.ts
@@ -30,33 +30,36 @@ async function main() {
     //     }
     // })
 
-    const mem = "maze ocean slab maximum sleep potato candy antique hammer parrot unable east"//generateMnemonic()
+    const mem = "maze ocean slab maximum sleep potato candy antique hammer parrot unable east"
+    // const mem = generateMnemonic()
     console.log(mem)
     const wallet = EthHdWallet.fromMnemonic(mem)
-    wallet.generateAddresses(2500)
-    let address = wallet.generateAddresses()
-    console.log(wallet.get)
 
-    let sql = "SELECT * FROM `whitelist`;"
-    let results: any[] = await new Promise<any[]>((resolve, reject) => {
+    let sql = "SELECT count(*) as count FROM `ico_addresses`;"
+    let count: number = await new Promise<number>((resolve, reject) => {
         db.query(sql, [], (error, results, fields) => {
             if (error) {
                 reject(error)
             }
-            resolve(results)
+            resolve(results[0].count)
         })
     })
-    for (let result of results) {
-        let idx = result['idx'];
-        setImmediate(addAddress, idx, utils.toChecksumAddress(address[idx]))
+    wallet.generateAddresses(count)
+    console.log(`DB Contains ${count} addresses`)
+    let newAddresses = 5
+    let addresses = wallet.generateAddresses(newAddresses)
+    console.log(`Generated ${addresses.length} addresses`)
+    let idx = count
+    for (let address of addresses) {
+        idx++
+        setImmediate(addAddress, idx, utils.toChecksumAddress(address))
     }
     console.log("Complete")
-
 }
 
 function addAddress(idx: number, address: string) {
-    let sql = "UPDATE `whitelist` set `address_eth` = ? where `idx` = ?;"
-    let q = db.query(sql, [address.toString(), idx], (error, results, fields) => {
+    let sql = "INSERT `ico_addresses` (idx, address_eth) VALUES (?, ?);"
+    let q = db.query(sql, [idx, address.toString()], (error, results, fields) => {
         if (error) {
             console.log(`Failed to give address '${address}' to ${idx}: ${error}`)
         } else {
@@ -69,4 +72,4 @@ function addAddress(idx: number, address: string) {
 
 main().catch((e) => {
     db.end();
-})
+}).then(() => console.log("Done"))
