9db97279784d9aec9dae595bd0a0eb078e769b44
Max Tang <maxtang@glosfer.com>
2018-04-20 19:34:09 +0900

    Change DB

--- src/dbConfig.ts
+++ src/dbConfig.ts
@@ -1,5 +1,5 @@
 const dbConfig = {
-    host: 'localhost',
+    host: 'hycon-io-live-cluster.cluster-cg8hfsnoeqkh.us-west-2.rds.amazonaws.com',
     port: 3306,
     user: 'infinity',
     password: 'wGt#X%d13t!!daoS%6pytOP8J75PLIAP',
